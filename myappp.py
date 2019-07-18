efd87689178bc1eaaa3f5fdf0c06985f88eafa2f
Enochsem <enochsem@gmail.com>
2019-07-18 01:45:29 -1200

    web application

---
+++ myappp.py
@@ -0,0 +1,23 @@
+from flask import *
+import json
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/foo/<name>')
+def foo(name):
+    return render_template('index.html', to=name)
+
+@app.route('/whereami')
+def whereami():
+    return 'Ghana!'
+
+if __name__ == '__main__':
+    port = os.environ.get('PORT',5000)
+    app.run(debug=True, host='0.0.0.0',port=port)
+    
+
